f8810ce8a6c0d600db0721594d81610f6cb7800a
ykore52 <ykore52@users.noreply.github.com>
2019-10-23 10:44:41 +0900

    update 'recq_add'

--- errbot/plugins/recbot/recbot.py
+++ errbot/plugins/recbot/recbot.py
@@ -17,6 +17,9 @@ REDIS_PORT = 6379
 
 REC_DIR = '/tmp'
 
+PYTHON_CMD = 'python3'
+AUTOEPG_PATH = '/opt/autoepg/autoepg.py'
+
 # 放送開始時間の N秒前に録画を開始する
 REC_START_OFFSET_SEC = 15
 
@@ -51,13 +54,17 @@ class Recbot(BotPlugin):
         * 録画キューの表示
                 !recq show
         * 録画キューの登録
-                !recq add 23 2019/10/17 1:00 [1:30]
+                !recq add 23 2019/10/17 1:00 [60]
+                開始時間のみが指定された場合は、番組表に番組が存在した場合のみキュー登録
+
         * 録画キューの自動録画登録
                 !recq autoadd 鉄腕ＤＡＳＨ
                 !recq autoadd /title 鉄腕ＤＡＳＨ
                 !recq autoadd /category バラエティ
         * 録画キューの削除
                 !recq del 1
+        * EPG データの更新
+                !recq update
         * 放送局コード表
                 {}
         '''.format(json.dump(channels))
@@ -193,31 +200,44 @@ class Recbot(BotPlugin):
     def recq_add(self, msg, args):
         args = args.split(' ')
         try:
-            timestamp = datetime.datetime.strptime
-            key = redis_client.get()
+            if len(args) != 3 and len(args) != 4:
+                return self.usage()
 
-            # 録画開始時間の調整
-            dt = datetime.datetime.strptime(args[1], '%Y/%m/%d %H:%M')
-            if REC_START_OFFSET_SEC >= 0:
-                dt = dt + datetime.timedelta(minutes=-int(REC_START_OFFSET_SEC/60)+1)
-                sleep_sec = 60 - REC_START_OFFSET_SEC
-            else:
-                dt = dt + datetime.timedelta(minutes=int(REC_START_OFFSET_SEC/60))
-                sleep_sec = REC_START_OFFSET_SEC
+            if len(args) == 3:
+                timestamp = datetime.datetime.strptime
+                key = redis_client.get()
 
-            if re.match('[0-9]+', args[0]):
-                ch = int(args[0])
-            duration = 0
-            ret = subprocess.run(
-                'echo "sleep {}; recpt1 --b25 --strip {} {} {}" '.format(sleep_sec, str(ch), str(duration - REC_END_OFFSET_SEC), os.path.join(REC_DIR, '')) +
-                '| at {0:2d}:{0:2d}'.format(dt.hour(), dt.minute()) +
-                ' {0:2d}{0:2d}{0:4d}'.format(dt.day(), dt.month(), dt.year())
-                , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                # 録画開始時間の調整
+                dt = datetime.datetime.strptime(args[1], '%Y/%m/%d %H:%M')
+                if REC_START_OFFSET_SEC >= 0:
+                    dt = dt + datetime.timedelta(minutes=-int(REC_START_OFFSET_SEC/60)+1)
+                    sleep_sec = 60 - REC_START_OFFSET_SEC
+                else:
+                    dt = dt + datetime.timedelta(minutes=int(REC_START_OFFSET_SEC/60))
+                    sleep_sec = REC_START_OFFSET_SEC
 
-            m = re.search('^job ([0-9]+) at .*?', ret.stdout, re.MULTILINE)
-            if m is None:
-                raise 'at コマンドの実行に失敗'
-            return '録画予約しました.\n' +
-                    '{}'.format(subprocess.run('at -l', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT))
+                if re.match('[0-9]+', args[0]):
+                    ch = int(args[0])
+                duration = 0
+                ret = subprocess.run(
+                    'echo "sleep {}; recpt1 --b25 --strip {} {} {}" '.format(sleep_sec, str(ch), str(duration - REC_END_OFFSET_SEC), os.path.join(REC_DIR, '')) +
+                    '| at {0:2d}:{0:2d}'.format(dt.hour(), dt.minute()) +
+                    ' {0:2d}{0:2d}{0:4d}'.format(dt.day(), dt.month(), dt.year())
+                    , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+                m = re.search('^job ([0-9]+) at .*?', ret.stdout, re.MULTILINE)
+                if m is None:
+                    raise 'at コマンドの実行に失敗'
+                return '録画予約しました.\n' +
+                        '{}'.format(subprocess.run('at -l', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT))
+
+            elif len(args) == 4:
+
         except Exception as e:
             return e
+
+    @botcmd
+    def recq_update(self, msg, args):
+        cmd = 'echo "{} {}" | at now + 1 min'.format(PYTHON_CMD, AUTOEPG_PATH)
+        ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        return 'EPG データ更新を1分後に予約しました.'
